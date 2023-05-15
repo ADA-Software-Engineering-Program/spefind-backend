9bf739143986666a1faf88be1a09e15e205be60d
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-05-15 10:00:59 +0100

    include sentry et al.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const { PORT } = require('./config/keys');
 const { connectToDatabase } = require('./config/mongoose');
 const { errorConverter, errorHandler } = require('./helpers/asyncError');
 const logger = require('./helpers/logger');
+const Sentry = require('@sentry/node');
 require('./auth/auth.service')(passport);
 const app = express();
 
 app.use(json());
-
 app.use(urlencoded({ extended: true }));
+Sentry.init({
+  dsn:
+    'https://f39ad17add1641d281c453b0bb74c3a0@o4503931293335552.ingest.sentry.io/4503931297136640',
+});
+app.use(Sentry.Handlers.requestHandler());
 
 app.use('/api/v1/', require('./routes/routes'));
 
@@ -20,11 +25,27 @@ app.get('/', (req, res) => {
     .json({ status: true, message: 'Welcome to MeetUp Server...' });
 });
 
-connectToDatabase();
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (
+        error.statusCode === 400 ||
+        error.statusCode === 500 ||
+        error.statusCode === 403 ||
+        error.statusCode === 404
+      ) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use(errorConverter);
 
 app.use(errorHandler);
+
+connectToDatabase();
 
 app.listen(PORT, () => {
   logger.info(`Server is now live at port ${PORT}`);
