8c6f0d80d598b881b7eee615191e2b8fe54964ba
Richie <molunorichie@gmail.com>
2023-06-24 22:19:25 +0100

    add: chat_model.js

---
+++ src/chat/chat.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chatRoomSchema = new Schema({
+    users: { type: [Schema.Types.ObjectId], ref: 'User', required: true },
+}, { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } });
+
+chatRoomSchema.virtual('messages', {
+    ref: 'Message',
+    localField: '_id',
+    foreignField: 'chat_room',
+    justOne: false,
+    options: { sort: { createdAt: -1 } },
+});
+
+const messageSchema = new Schema({
+    sender: { type: Schema.Types.ObjectId, ref: 'User' },
+    chat_room: { type: Schema.Types.ObjectId, ref: 'ChatRoom' },
+    message: { type: String, required: true },
+}, { timestamps: true });
+
+const ChatRoom = mongoose.model('ChatRoom', chatRoomSchema);
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = { ChatRoom, Message };
