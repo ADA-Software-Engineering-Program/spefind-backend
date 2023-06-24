b5511a7b532485f772292f9bdb870accaa1a0e97
Richie <molunorichie@gmail.com>
2023-06-24 23:03:02 +0100

    update imports

--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -2,182 +2,182 @@ const catchAsync = require('express-async-handler');
 const tokenService = require('./token.service');
 const cron = require('node-cron');
 const passport = require('passport');
-const ApiError = require('../helpers/error');
+const ApiError = require('../helpers/error').default;
 const authService = require('./auth.services');
 const { sendOTP, resendOTPMail } = require('../helpers/email');
 const cloudinary = require('../helpers/cloudinary');
 
 const register = catchAsync(async (req, res) => {
-  const data = await authService.registerUser(req.body);
-  const authToken = await tokenService.generateAuthTokens(data);
-  const token = authToken.access.token;
-  sendOTP(data.email, data.userPin);
-  res.status(201).json({
-    status: true,
-    message:
-      'Account Creation Initiated! You have also just received an OTP in your mail...',
-    data,
-    token,
-  });
+    const data = await authService.registerUser(req.body);
+    const authToken = await tokenService.generateAuthTokens(data);
+    const token = authToken.access.token;
+    sendOTP(data.email, data.userPin);
+    res.status(201).json({
+        status: true,
+        message:
+            'Account Creation Initiated! You have also just received an OTP in your mail...',
+        data,
+        token,
+    });
 });
 
 const confirmOTP = catchAsync(async (req, res) => {
-  if (!req.body.OTP) {
-    throw new ApiError(400, 'An OTP is required here...');
-  }
-  // console.log(req.user);
-  // console.log(req.body);
-  await authService.confirmOTP(req.user._id, req.body.OTP);
-  res.status(200).json({
-    status: true,
-    message: 'Yeaa! OTP correctly inputted... Your email is also now verified!',
-  });
+    if (!req.body.OTP) {
+        throw new ApiError(400, 'An OTP is required here...');
+    }
+    // console.log(req.user);
+    // console.log(req.body);
+    await authService.confirmOTP(req.user._id, req.body.OTP);
+    res.status(200).json({
+        status: true,
+        message: 'Yeaa! OTP correctly inputted... Your email is also now verified!',
+    });
 });
 
 const resendOTP = catchAsync(async (req, res) => {
-  const data = await authService.resendOTP(req.user._id);
-  resendOTPMail(req.user.email, data);
-  res.status(201).json({
-    status: true,
-    message: 'OTP has just been resent to your mail...',
-  });
+    const data = await authService.resendOTP(req.user._id);
+    resendOTPMail(req.user.email, data);
+    res.status(201).json({
+        status: true,
+        message: 'OTP has just been resent to your mail...',
+    });
 });
 
 const setupProfile = catchAsync(async (req, res) => {
-  let requestBody = req.body;
-  if (req.file) {
-    const avatar = await cloudinary.uploader.upload(req.file.path);
-    requestBody.thumbNail = avatar.secure_url;
-  }
-  const data = await authService.setProfile(req.user._id, requestBody);
-  const token = await tokenService.generateAuthTokens(data);
-  res.status(201).json({
-    status: 'success',
-    message: 'First, Last Name, Username & Password et al. now Updated...',
-    data,
-    token: token.access.token,
-  });
+    let requestBody = req.body;
+    if (req.file) {
+        const avatar = await cloudinary.uploader.upload(req.file.path);
+        requestBody.thumbNail = avatar.secure_url;
+    }
+    const data = await authService.setProfile(req.user._id, requestBody);
+    const token = await tokenService.generateAuthTokens(data);
+    res.status(201).json({
+        status: 'success',
+        message: 'First, Last Name, Username & Password et al. now Updated...',
+        data,
+        token: token.access.token,
+    });
 });
 
 const login = catchAsync((req, res, next) => {
-  passport.authenticate('login', async (err, user, info) => {
-    try {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        const err = new ApiError(
-          400,
-          'Ooopss! You have either inputted an incorrect password or an unregistered email...'
-        );
-        return next(err);
-      }
-      req.login(user, { session: false }, async (err) => {
-        if (err) return next(err);
-        const data = {
-          _id: user._id,
-          email: user.email,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          username: user.username,
-        };
-        const token = await tokenService.generateAuthTokens(data);
+    passport.authenticate('login', async (err, user, info) => {
+        try {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                const err = new ApiError(
+                    400,
+                    'Ooopss! You have either inputted an incorrect password or an unregistered email...'
+                );
+                return next(err);
+            }
+            req.login(user, { session: false }, async (err) => {
+                if (err) return next(err);
+                const data = {
+                    _id: user._id,
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    username: user.username,
+                };
+                const token = await tokenService.generateAuthTokens(data);
 
-        res.status(200).json({
-          status: 'success',
-          message: 'Login Successful!',
-          data,
-          token: token.access.token,
-        });
-      });
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
+                res.status(200).json({
+                    status: 'success',
+                    message: 'Login Successful!',
+                    data,
+                    token: token.access.token,
+                });
+            });
+        } catch (error) {
+            return next(error);
+        }
+    })(req, res, next);
 });
 
 const userBio = async (req, res) => {
-  const data = await authService.userBio(req.user._id, req.body);
-  res
-    .status(200)
-    .json({ status: true, message: 'User Bio Now Updated...', data });
+    const data = await authService.userBio(req.user._id, req.body);
+    res
+        .status(200)
+        .json({ status: true, message: 'User Bio Now Updated...', data });
 };
 
 const forgotPassword = async (req, res) => {
-  const { email } = await authService.getUserByMail(req.body.email);
-  let data = { email };
-  const token = await tokenService.generateAuthTokens(data);
+    const { email } = await authService.getUserByMail(req.body.email);
+    let data = { email };
+    const token = await tokenService.generateAuthTokens(data);
 
-  res.status(200).json({
-    status: true,
-    message: 'Email confirmed... Please proceed onto reset your password',
-    token: token.access.token,
-  });
+    res.status(200).json({
+        status: true,
+        message: 'Email confirmed... Please proceed onto reset your password',
+        token: token.access.token,
+    });
 };
 
 const resetPassword = catchAsync(async (req, res) => {
-  await authService.changePassword(req.user.email, req.body.password);
-  res.status(200).json({
-    status: 'success',
-    message: 'Password successfully reset...',
-  });
+    await authService.changePassword(req.user.email, req.body.password);
+    res.status(200).json({
+        status: 'success',
+        message: 'Password successfully reset...',
+    });
 });
 
 const changePassword = catchAsync(async (req, res) => {
-  if (!req.body.password) {
-    throw new ApiError(400, 'Kindly input the new desired password');
-  }
-  const user = await authService.changePassword(
-    req.user.email,
-    req.body.password
-  );
-  res.status(200).json({
-    status: true,
-    message: 'Password Change Successfully Effected...',
-  });
+    if (!req.body.password) {
+        throw new ApiError(400, 'Kindly input the new desired password');
+    }
+    const user = await authService.changePassword(
+        req.user.email,
+        req.body.password
+    );
+    res.status(200).json({
+        status: true,
+        message: 'Password Change Successfully Effected...',
+    });
 });
 
 const editProfile = catchAsync(async (req, res) => {
-  if (req.body.email) {
-    throw new ApiError(400, " Oops! You can't update your email Here!");
-  }
-  if (req.body.password) {
-    throw new ApiError(400, "You can't update your password Here!");
-  }
-  const updatedbody = req.body;
+    if (req.body.email) {
+        throw new ApiError(400, " Oops! You can't update your email Here!");
+    }
+    if (req.body.password) {
+        throw new ApiError(400, "You can't update your password Here!");
+    }
+    const updatedbody = req.body;
 
-  if (req.file) {
-    const avatar = await cloudinary.uploader.upload(req.file.path);
-    updatedbody.thumbNail = avatar.secure_url;
-  }
+    if (req.file) {
+        const avatar = await cloudinary.uploader.upload(req.file.path);
+        updatedbody.thumbNail = avatar.secure_url;
+    }
 
-  const user = await authService.editUserProfile(req.user._id, updatedbody);
-  res.status(200).json({
-    status: 'success',
-    message: 'Yeaa! Profile update successful!',
-    user,
-  });
+    const user = await authService.editUserProfile(req.user._id, updatedbody);
+    res.status(200).json({
+        status: 'success',
+        message: 'Yeaa! Profile update successful!',
+        user,
+    });
 });
 
 const updateField = catchAsync(async (req, res) => {
-  const data = await authService.updateField(req.user._id, req.body);
-  res.status(200).json({
-    status: true,
-    message: 'Field of Discipline now updated...',
-    data,
-  });
+    const data = await authService.updateField(req.user._id, req.body);
+    res.status(200).json({
+        status: true,
+        message: 'Field of Discipline now updated...',
+        data,
+    });
 });
 
 module.exports = {
-  register,
-  updateField,
-  setupProfile,
-  login,
-  editProfile,
-  forgotPassword,
-  confirmOTP,
-  userBio,
-  resendOTP,
-  resetPassword,
-  changePassword,
+    register,
+    updateField,
+    setupProfile,
+    login,
+    editProfile,
+    forgotPassword,
+    confirmOTP,
+    userBio,
+    resendOTP,
+    resetPassword,
+    changePassword,
 };
--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -1,5 +1,5 @@
 const User = require('./user.model');
-const ApiError = require('../helpers/error');
+const ApiError = require('../helpers/error').default;
 const Following = require('../user/following.model');
 const Follow = require('../user/follow.model');
 const bcrypt = require('bcryptjs');
@@ -7,197 +7,197 @@ const cron = require('node-cron');
 const { followNotification } = require('../helpers/email');
 
 const registerUser = async (data) => {
-  const code = Math.floor(Math.random() * (999999 - 100000) + 100000);
-  data.userPin = code;
-  const rawData = JSON.parse(JSON.stringify(data));
-  const returnedData = await User.create(data);
-  const user = { userId: returnedData._id };
-  await Follow.create(user);
-  await Following.create(user);
-  const refreshCode = Math.floor(Math.random() * (999999 - 100000) + 100000);
-  // cron.schedule('*/5 * * * *', async () => {
-  //   await User.findByIdAndUpdate(
-  //     returnedData._id,
-  //     { userPin: refreshCode },
-  //     { new: true }
-  //   );
-  // });
-  return returnedData;
+    const code = Math.floor(Math.random() * (999999 - 100000) + 100000);
+    data.userPin = code;
+    const rawData = JSON.parse(JSON.stringify(data));
+    const returnedData = await User.create(data);
+    const user = { userId: returnedData._id };
+    await Follow.create(user);
+    await Following.create(user);
+    const refreshCode = Math.floor(Math.random() * (999999 - 100000) + 100000);
+    // cron.schedule('*/5 * * * *', async () => {
+    //   await User.findByIdAndUpdate(
+    //     returnedData._id,
+    //     { userPin: refreshCode },
+    //     { new: true }
+    //   );
+    // });
+    return returnedData;
 };
 
 const setProfile = async (userId, data) => {
-  try {
-    const rawData = data;
-    let hashedPassword;
+    try {
+        const rawData = data;
+        let hashedPassword;
 
-    if (data.thumbNail) {
-      return await User.findByIdAndUpdate(userId, data, { new: true });
+        if (data.thumbNail) {
+            return await User.findByIdAndUpdate(userId, data, { new: true });
+        }
+
+        if (data.password) hashedPassword = await bcrypt.hash(data.password, 10);
+        rawData.password = hashedPassword;
+        const { followings, ...refinedData } = data;
+
+        for (let i = 0; i < followings.length; i++) {
+            await Follow.findOneAndUpdate(
+                { userId: followings[i] },
+                { $push: { followers: userId } },
+                { new: true }
+            );
+
+            const newNumberOfFollowings = followings.length;
+
+            await User.findByIdAndUpdate(
+                userId,
+                { numberOfFollowings: newNumberOfFollowings },
+                { new: true }
+            );
+            await User.findByIdAndUpdate(
+                userId,
+                { isProfileCreationComplete: true },
+                { new: true }
+            );
+
+            const { numberOfFollowers } = await User.findById(followings[i]);
+
+            const newNumberOfFollowers = numberOfFollowers + 1;
+
+            await User.findByIdAndUpdate(
+                followings[i],
+                { numberOfFollowers: newNumberOfFollowers },
+                { new: true }
+            );
+
+            const { firstName, email } = await User.findById(followings[i]);
+
+            followNotification(email, firstName, data.firstName, data.lastName);
+        }
+
+        await Following.findOneAndUpdate(
+            { userId: userId },
+            { following: followings },
+            {
+                new: true,
+            }
+        );
+
+        const outputData = await User.findByIdAndUpdate(
+            userId,
+            { $set: refinedData },
+            { new: true }
+        );
+
+        return {
+            _id: outputData._id,
+            email: outputData.email,
+            firstName: outputData.firstName,
+            lastName: outputData.lastName,
+            thumbNail: outputData.thumbNail,
+            username: outputData.username,
+        };
+    } catch (error) {
+        throw new ApiError(400, 'Unable to input user information');
     }
-
-    if (data.password) hashedPassword = await bcrypt.hash(data.password, 10);
-    rawData.password = hashedPassword;
-    const { followings, ...refinedData } = data;
-
-    for (let i = 0; i < followings.length; i++) {
-      await Follow.findOneAndUpdate(
-        { userId: followings[i] },
-        { $push: { followers: userId } },
-        { new: true }
-      );
-
-      const newNumberOfFollowings = followings.length;
-
-      await User.findByIdAndUpdate(
-        userId,
-        { numberOfFollowings: newNumberOfFollowings },
-        { new: true }
-      );
-      await User.findByIdAndUpdate(
-        userId,
-        { isProfileCreationComplete: true },
-        { new: true }
-      );
-
-      const { numberOfFollowers } = await User.findById(followings[i]);
-
-      const newNumberOfFollowers = numberOfFollowers + 1;
-
-      await User.findByIdAndUpdate(
-        followings[i],
-        { numberOfFollowers: newNumberOfFollowers },
-        { new: true }
-      );
-
-      const { firstName, email } = await User.findById(followings[i]);
-
-      followNotification(email, firstName, data.firstName, data.lastName);
-    }
-
-    await Following.findOneAndUpdate(
-      { userId: userId },
-      { following: followings },
-      {
-        new: true,
-      }
-    );
-
-    const outputData = await User.findByIdAndUpdate(
-      userId,
-      { $set: refinedData },
-      { new: true }
-    );
-
-    return {
-      _id: outputData._id,
-      email: outputData.email,
-      firstName: outputData.firstName,
-      lastName: outputData.lastName,
-      thumbNail: outputData.thumbNail,
-      username: outputData.username,
-    };
-  } catch (error) {
-    throw new ApiError(400, 'Unable to input user information');
-  }
 };
 
 const confirmOTP = async (userId, data) => {
-  try {
-    const { userPin } = await User.findById(userId);
+    try {
+        const { userPin } = await User.findById(userId);
 
-    if (userPin != data) {
-      throw new ApiError(400, 'Incorrect pin inputted...');
+        if (userPin != data) {
+            throw new ApiError(400, 'Incorrect pin inputted...');
+        }
+
+        return await User.findByIdAndUpdate(
+            userId,
+            { isAccountConfirmed: true },
+            { new: true }
+        );
+    } catch (error) {
+        throw new ApiError(400, 'Incorrect pin inputted...');
     }
-
-    return await User.findByIdAndUpdate(
-      userId,
-      { isAccountConfirmed: true },
-      { new: true }
-    );
-  } catch (error) {
-    throw new ApiError(400, 'Incorrect pin inputted...');
-  }
 };
 
 const resendOTP = async (userId) => {
-  const code = Math.floor(Math.random() * (999999 - 100000) + 100000);
-  const { userPin } = await User.findById(userId);
-  cron.schedule('*/5 * * * *', async () => {
-    await User.findByIdAndUpdate(userId, { userPin: code }, { new: true });
-  });
+    const code = Math.floor(Math.random() * (999999 - 100000) + 100000);
+    const { userPin } = await User.findById(userId);
+    cron.schedule('*/5 * * * *', async () => {
+        await User.findByIdAndUpdate(userId, { userPin: code }, { new: true });
+    });
 
-  return userPin;
+    return userPin;
 };
 
 const updateUserById = async (id, data) => {
-  try {
-    return await User.findByIdAndUpdate(id, data, { new: true });
-  } catch (error) {
-    throw new ApiError(400, 'Unable to update user by Id');
-  }
+    try {
+        return await User.findByIdAndUpdate(id, data, { new: true });
+    } catch (error) {
+        throw new ApiError(400, 'Unable to update user by Id');
+    }
 };
 
 const userBio = async (userId, info) => {
-  try {
-    return await updateUserById(userId, info);
-  } catch (error) {
-    throw new ApiError(400, 'Unable to update User Bio...');
-  }
+    try {
+        return await updateUserById(userId, info);
+    } catch (error) {
+        throw new ApiError(400, 'Unable to update User Bio...');
+    }
 };
 
 const getUserByMail = async (email) => {
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw new ApiError(400, 'You are yet to be registered with this mail...');
-    }
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new ApiError(400, 'You are yet to be registered with this mail...');
+        }
 
-    return JSON.parse(JSON.stringify(user));
-  } catch (err) {
-    throw new ApiError(
-      400,
-      'Oops! You are yet to be registered with this mail...'
-    );
-  }
+        return JSON.parse(JSON.stringify(user));
+    } catch (err) {
+        throw new ApiError(
+            400,
+            'Oops! You are yet to be registered with this mail...'
+        );
+    }
 };
 
 const changePassword = async (user, data) => {
-  let hashedPassword = await bcrypt.hash(data, 10);
-  return await User.findOneAndUpdate(
-    { email: user },
-    { password: hashedPassword },
-    { new: true }
-  );
+    let hashedPassword = await bcrypt.hash(data, 10);
+    return await User.findOneAndUpdate(
+        { email: user },
+        { password: hashedPassword },
+        { new: true }
+    );
 };
 
 const comparePassword = async (entered, password) => {
-  try {
-    const result = await bcrypt.compare(entered, password);
-    if (!result) {
-      throw new ApiError(400, 'Oops! Inputted password is incorrect!');
+    try {
+        const result = await bcrypt.compare(entered, password);
+        if (!result) {
+            throw new ApiError(400, 'Oops! Inputted password is incorrect!');
+        }
+        return result;
+    } catch (err) {
+        throw new ApiError(400, 'Incorrect old password inputted...');
     }
-    return result;
-  } catch (err) {
-    throw new ApiError(400, 'Incorrect old password inputted...');
-  }
 };
 
 const editUserProfile = async (userId, data) => {
-  const user = await User.findOne({ _id: userId });
-  if (!user) {
-    throw new ApiError(400, 'User not found...');
-  }
-  // return await User.findByIdAndUpdate(userId, data, { new: true });
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+        throw new ApiError(400, 'User not found...');
+    }
+    // return await User.findByIdAndUpdate(userId, data, { new: true });
 
-  // throw new ApiError(400, "Kindly input the details you're looking to update!");
+    // throw new ApiError(400, "Kindly input the details you're looking to update!");
 };
 
 module.exports = {
-  registerUser,
-  setProfile,
-  userBio,
-  editUserProfile,
-  confirmOTP,
-  getUserByMail,
-  resendOTP,
-  changePassword,
+    registerUser,
+    setProfile,
+    userBio,
+    editUserProfile,
+    confirmOTP,
+    getUserByMail,
+    resendOTP,
+    changePassword,
 };
