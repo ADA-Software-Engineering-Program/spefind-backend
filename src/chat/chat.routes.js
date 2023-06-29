cc09b3a2f9fbd10bb88dd1b018b56120df2f8570
Richie <molunorichie@gmail.com>
2023-06-29 17:21:38 +0100

    udpate chat route

--- src/chat/chat.routes.js
+++ src/chat/chat.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
+const { getChatRoomMessages } = require('./chat.controller');
+const { userAuthentication, checkUserExistence } = require('../helpers/auth');
 const router = Router()
 
-router  
-    .get('/history', getChatHistory)
+router.use(userAuthentication)
 
+router
+    .get('/history', getChatHistory)
+    .get('/history/specific', getSpecificChatHistory)
+    .get('/messages', getChatRoomMessages)
 
 module.exports = router
