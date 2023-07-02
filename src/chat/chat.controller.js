f559b2c123b4a584ed3cb115da089ed1861dcb1c
Richie <molunorichie@gmail.com>
2023-07-02 01:11:05 +0100

    add: catchAsync wrapper to chat.controller

--- src/chat/chat.controller.js
+++ src/chat/chat.controller.js
@@ -3,44 +3,36 @@ const { BadRequestError } = require("../helpers/error")
 const { ChatRoom } = require("./chat.model")
 const { getPreviousMessages } = require("./chat.service")
 const catchAsync = require('express-async-handler');
-const ApiError = require('../helpers/error').default;
 
 const createNewChatRoom = catchAsync(async (req, res, next) => {
     const { user_ids } = req.body
 
     if (user_ids.length > 2) {
-        console.log('error')
-        const error = new BadRequestError('Only 2 users can be in a chat room')
-        console.log(error.statusCode, error.message)
-        throw new ApiError(400, 'Only 2 users can be in a chat room')
+        throw new BadRequestError('Only 2 users can be in a chat room')
     }
 
-    // const existing_users = await User.find({ _id: { $in: user_ids } })
-    // const not_all_users_exist = existing_users.length < user_ids.length
-    // if (not_all_users_exist) {
-    //     return res.status(400).send({
-    //         message: 'Not all users exist'
-    //     })
-    // }
+    const existing_users = await User.find({ _id: { $in: user_ids } })
+    const not_all_users_exist = existing_users.length < user_ids.length
+    if (not_all_users_exist) {
+        throw new BadRequestError('Not all users exist')
+    }
 
-    // const users_above_chatroom_limit = existing_users.length > 2
-    // if (users_above_chatroom_limit) {
-    //     return res.status(400).send({
-    //         message: 'Only 2 users can be in a chatroom'
-    //     })
-    // }
+    const users_above_chatroom_limit = existing_users.length > 2
+    if (users_above_chatroom_limit) {
+        throw new BadRequestError('Only 2 users can be in a chatroom')
+    }
 
-    // const chat_room = await ChatRoom.create({
-    //     users: [user_ids]
-    // })
+    const chat_room = await ChatRoom.create({
+        users: [user_ids]
+    })
 
-    // return res.status(200).json({
-    //     sucess: true,
-    //     message: 'Chatroom created successfully',
-    //     data: {
-    //         chat_room
-    //     }
-    // })
+    return res.status(200).json({
+        sucess: true,
+        message: 'Chatroom created successfully',
+        data: {
+            chat_room
+        }
+    })
 })
 
 const getChatRoomMessages = catchAsync(async (req, res, next) => {
--- src/helpers/asyncError.js
+++ src/helpers/asyncError.js
@@ -1,7 +1,6 @@
 const logger = require('./logger');
-const ApiError = require('./error');
+const ApiError = require('./error').default;
 const errorConverter = (err, req, res, next) => {
-    logger.info(err.message, err.name)
   let error = err;
   if (!(error instanceof ApiError)) {
     const statusCode = error.statusCode || 500;
